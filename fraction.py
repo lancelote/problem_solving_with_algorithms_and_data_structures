772f1488964bb7de9825e4018b1f73984ddfdd36
lancelote <lancelote.du.lac@gmail.com>
2015-03-04 18:35:10 +0300

    add, str methods, init method update, gcd def, testing

--- fraction.py
+++ fraction.py
@@ -1,8 +1,28 @@
+def gcd(m, n):
+    while m % n != 0:
+        m, n = n, m % n
+    return n
+
+
 class Fraction:
 
     def __init__(self, top, bottom):
         if type(top) == int and type(bottom) == int:
-            self.num = top
-            self.den = bottom
+            if bottom < 0:
+                raise ValueError("Negative fraction should have negative numerator not denominator")
+            elif top == 0 or bottom == 0:
+                raise ValueError("Denominator and numerator should not be equal to zero")
+            else:
+                self.num = top
+                self.den = bottom
         else:
             raise TypeError("Wrong input type: {} and {}".format(type(top), type(bottom)))
+
+    def __str__(self):
+        return str(self.num) + "/" + str(self.den)
+
+    def __add__(self, other):
+        new_num = self.num*other.den + other.num*self.den
+        new_den = self.den*other.den
+        reduction = gcd(new_num, new_den)
+        return Fraction(new_num//reduction, new_den//reduction)
--- fraction_test.py
+++ fraction_test.py
@@ -1,5 +1,14 @@
 import unittest
 from fraction import Fraction
+from fraction import gcd
+
+
+class GcdTest(unittest.TestCase):
+
+    def gcd_returns_correct_value(self):
+        self.assertEqual(gcd(20, 10), 10)
+        self.assertEqual(gcd(17, 13), 1)
+        self.assertEqual(gcd(18, 24), 6)
 
 
 class FractionTest(unittest.TestCase):
@@ -7,10 +16,22 @@ class FractionTest(unittest.TestCase):
     def setUp(self):
         self.test_fraction = Fraction(1, 2)
 
-    def test_init(self):
+    def test_init_works_correct(self):
         self.assertEqual(self.test_fraction.num, 1)
         self.assertEqual(self.test_fraction.den, 2)
+
+    def test_init_raises_typeerror_if_not_int(self):
         self.assertRaises(TypeError, self.test_fraction, "Hello", "World")
 
-    def test_str(self):
-        self.assertEqual(self.test_fraction.__str__, "1/2")
+    def test_init_raise_valueerror_if_den_is_negative(self):
+        self.assertRaises(ValueError, Fraction, 1, -1)
+
+    def test_init_raise_valueerror_if_den_or_num_is_zero(self):
+        self.assertRaises(ValueError, Fraction, 0, 1)
+        self.assertRaises(ValueError, Fraction, 1, 0)
+
+    def test_str_returns_correct_value(self):
+        self.assertEqual(self.test_fraction.__str__(), "1/2")
+
+    def test_add_returns_correct_value(self):
+        self.assertEqual((self.test_fraction + Fraction(2, 5)).__str__(), "9/10")

e3f1220c4629a7adfafe8294624af7957d7860c6
lancelote <lancelote.du.lac@gmail.com>
2015-03-05 18:14:23 +0300

    digital curcuit implementation

---
+++ digital_curcuit.py
@@ -0,0 +1,68 @@
+class LogicGate:
+
+    def __init__(self, n):
+        self.label = n
+        self.output = None
+
+    def perform_gate_logic(self):
+        pass
+
+    def get_label(self):
+        return self.label
+
+    def get_output(self):
+        self.output = self.perform_gate_logic()
+        return self.output
+
+
+class BinaryGate(LogicGate):
+
+    def __init__(self, n):
+        LogicGate.__init__(self, n)
+
+        self.pin_a = None
+        self.pin_b = None
+
+    def get_pin_a(self):
+        return self.pin_a
+
+    def get_pin_b(self):
+        return self.pin_b
+
+    def set_pin_a(self, n):
+        if n != 0 and n != 1:
+            raise ValueError("Pin A should be 0 or 1")
+        else:
+            self.pin_a = n
+
+    def set_pin_b(self, n):
+        if n != 0 and n != 1:
+            raise ValueError("Pin B should be 0 or 1")
+        else:
+            self.pin_b = n
+
+
+class UnaryGate(LogicGate):
+
+    def __init__(self, n):
+        LogicGate.__init__(self, n)
+
+        self.pin = None
+
+    def get_pin(self):
+        return self.pin
+
+    def set_pin(self, n):
+        if n != 0 and n != 1:
+            raise ValueError("Pin should be 0 or 1")
+        else:
+            self.pin = n
+
+
+class AndGate(BinaryGate):
+
+    def __init__(self, n):
+        BinaryGate.__init__(self, n)
+
+    def perform_gate_logic(self):
+        pass
---
+++ digital_curcuit_test.py
@@ -0,0 +1,103 @@
+import unittest
+from digital_curcuit import LogicGate, BinaryGate, UnaryGate, AndGate
+
+
+class LogicGateTest(unittest.TestCase):
+
+    def setUp(self):
+        self.test_logic_gate = LogicGate("Label")
+
+    def test_init_creates_correct_object(self):
+        self.assertEqual(self.test_logic_gate.label, "Label")
+        self.assertIsNone(self.test_logic_gate.output)
+
+    def test_get_label_returns_correct_result(self):
+        self.assertEqual(self.test_logic_gate.get_label(), "Label")
+
+    def test_get_output_returns_correct_result(self):
+        self.assertIsNone(self.test_logic_gate.get_output())
+
+
+class BinaryGateTest(unittest.TestCase):
+
+    def setUp(self):
+        self.test_binary_gate = BinaryGate("Label")
+
+    def test_init_creates_correct_object(self):
+        self.assertIsNone(self.test_binary_gate.pin_a)
+        self.assertIsNone(self.test_binary_gate.pin_b)
+
+    def test_set_pin_a_works_correct(self):
+        self.test_binary_gate.set_pin_a(0)
+        self.assertEqual(self.test_binary_gate.pin_a, 0)
+        self.test_binary_gate.set_pin_a(1)
+        self.assertEqual(self.test_binary_gate.pin_a, 1)
+
+    def test_set_pin_b_works_correct(self):
+        self.test_binary_gate.set_pin_b(0)
+        self.assertEqual(self.test_binary_gate.pin_b, 0)
+        self.test_binary_gate.set_pin_b(1)
+        self.assertEqual(self.test_binary_gate.pin_b, 1)
+
+    def test_set_pin_a_raises_valuerror_if_not_0_or_1(self):
+        self.assertRaises(ValueError, self.test_binary_gate.set_pin_a, "Hello")
+
+    def test_set_pin_b_raises_valuerror_if_not_0_or_1(self):
+        self.assertRaises(ValueError, self.test_binary_gate.set_pin_b, "Hello")
+
+    def test_get_pin_a_returns_correct_result(self):
+        self.assertIsNone(self.test_binary_gate.get_pin_a())
+        self.test_binary_gate.set_pin_a(0)
+        self.assertEqual(self.test_binary_gate.get_pin_a(), 0)
+        self.test_binary_gate.set_pin_a(1)
+        self.assertEqual(self.test_binary_gate.get_pin_a(), 1)
+
+    def test_get_pin_b_returns_correct_result(self):
+        self.assertIsNone(self.test_binary_gate.get_pin_b())
+        self.test_binary_gate.set_pin_b(0)
+        self.assertEqual(self.test_binary_gate.get_pin_b(), 0)
+        self.test_binary_gate.set_pin_b(1)
+        self.assertEqual(self.test_binary_gate.get_pin_b(), 1)
+
+
+class UnaryGateTest(unittest.TestCase):
+
+    def setUp(self):
+        self.test_unary_gate = UnaryGate("Label")
+
+    def test_init_creates_correct_object(self):
+        self.assertIsNone(self.test_unary_gate.pin)
+
+    def test_set_pin_works_correct(self):
+        self.test_unary_gate.set_pin(0)
+        self.assertEqual(self.test_unary_gate.pin, 0)
+        self.test_unary_gate.set_pin(1)
+        self.assertEqual(self.test_unary_gate.pin, 1)
+
+    def test_set_pin_raises_valuerror_if_not_0_or_1(self):
+        self.assertRaises(ValueError, self.test_unary_gate.set_pin, "Hello")
+
+    def test_get_pin_returns_correct_result(self):
+        self.assertIsNone(self.test_unary_gate.get_pin())
+        self.test_unary_gate.set_pin(0)
+        self.assertEqual(self.test_unary_gate.get_pin(), 0)
+        self.test_unary_gate.set_pin(1)
+        self.assertEqual(self.test_unary_gate.get_pin(), 1)
+
+
+class AndGateTest(unittest.TestCase):
+
+    def setUp(self):
+        self.test_and_gate = AndGate("Label")
+
+    def test_perform_gate_logic_returns_correct_result(self):
+        self.test_and_gate.set_pin_a(0)
+        self.test_and_gate.set_pin_b(0)
+        self.assertEqual(self.test_and_gate.perform_gate_logic(), 0)
+        self.test_and_gate.set_pin_a(1)
+        self.assertEqual(self.test_and_gate.perform_gate_logic(), 0)
+        self.test_and_gate.set_pin_a(0)
+        self.test_and_gate.set_pin_b(1)
+        self.assertEqual(self.test_and_gate.perform_gate_logic(), 0)
+        self.test_and_gate.set_pin_a(1)
+        self.assertEqual(self.test_and_gate.perform_gate_logic(), 1)

9047bc9fd213a5b60c33518f19940affc64a0d9b
lancelote <lancelote.du.lac@gmail.com>
2015-03-18 20:50:11 +0300

    anagram_4

--- anagram/anagram_test.py
+++ anagram/anagram_test.py
@@ -1,5 +1,5 @@
 import unittest
-from anagram.anagram import anagram_1, anagram_2, anagram_3
+from anagram.anagram import anagram_1, anagram_2, anagram_3, anagram_4
 
 
 class AnagramTest(unittest.TestCase):
@@ -12,6 +12,10 @@ class AnagramTest(unittest.TestCase):
         self.assertTrue(anagram_2("abcd", "dcba"))
         self.assertFalse(anagram_2("abcde", "dcbaf"))
 
-    def test_anagtam_3_returns_correct_result(self):
+    def test_anagram_3_returns_correct_result(self):
         self.assertTrue(anagram_3("abcd", "dcba"))
         self.assertFalse(anagram_3("abcde", "dcbaf"))
+
+    def test_anagram_4_returns_correct_result(self):
+        self.assertTrue(anagram_4("abcd", "dcba"))
+        self.assertFalse(anagram_4("abcde", "dcbaf"))
